c067b7712d7ae0a749d9dbae0d85837a01b6ceda
dujianzhong <250611729@qq.com>
2021-01-26 10:41:43 +0800

    1111

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  // mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/components/Layout'),
+      children:[
+        {
+          path: '',
+          name: 'home',
+          component: () => import('@/views/Home')
+        },
+        {
+          path: '/home',
+          name: 'test',
+          component: () => import('@/views/Home')
+        }
+      ]
+    },
+    {
+      path:'/login',
+      name:'login',
+      meta:{
+        title:'登录',
+      },
+      component: () => import('@/views/Login')
+    },
+  ]
+})
